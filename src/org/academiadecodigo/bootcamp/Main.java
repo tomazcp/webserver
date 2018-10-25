4ac88d68cf6581f833f50cbebb62d6e262b96e73
Tomás Pinto <tomaspinto@protonmail.ch>
2018-10-25 21:44:05 +0100

    server starting

--- src/org/academiadecodigo/bootcamp/Main.java
+++ src/org/academiadecodigo/bootcamp/Main.java
@@ -1,8 +1,12 @@
 package org.academiadecodigo.bootcamp;
 
+import org.academiadecodigo.bootcamp.server.WebServer;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        int port = 8080;
+        WebServer webServer = new WebServer(port);
+        webServer.start();
     }
 }
