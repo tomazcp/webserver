3fe6ef08f19054ebd77a866d9dd2fe5840a85553
Tomás <tomaspinto@protonmail.ch>
2018-10-30 15:59:22 +0000

    now supports multiple connections

--- src/org/academiadecodigo/bootcamp/server/WebServer.java
+++ src/org/academiadecodigo/bootcamp/server/WebServer.java
@@ -3,8 +3,6 @@ package org.academiadecodigo.bootcamp.server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public class WebServer {
@@ -13,6 +11,7 @@ public class WebServer {
 
     private int port;
     private ServerSocket socket;
+
 
     public WebServer(int port) {
         this.port = port;
@@ -26,7 +25,8 @@ public class WebServer {
             socket = new ServerSocket(port);
 
             while (true) {
-                listen();
+                Thread thread = new Thread(new RequestHandler(socket.accept()));
+                thread.start();
             }
 
         } catch (IOException ex) {
@@ -34,96 +34,115 @@ public class WebServer {
         }
     }
 
-    /**
-     * Listens for requests
-     */
-    private void listen() {
-        try {
-            Socket clientSocket = socket.accept();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            Request req = newRequest(reader.readLine());
-            Response res = processRequest(req);
-            DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+    private class RequestHandler implements Runnable {
 
-            send(res, out);
-            clientSocket.close();
+        private Socket clientSocket;
 
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        RequestHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
         }
-    }
 
-    /**
-     * Creates a new request object from the incoming header
-     *
-     * @param header the request header
-     * @return
-     */
-    private Request newRequest(String header) {
-        String[] headerInfo = header.split(" ");
-        return new Request(headerInfo[0], headerInfo[1]);
-    }
-
-    /**
-     *
-     * @param req
-     * @return
-     */
-    private Response processRequest(Request req) {
-        Response res = null;
-        switch (req.getRequestType()) {
-            case GET:
-                File file = fetchResources(req.getResource());
-                if (file.exists()) {
-                    ResponseHeader responseHeader =
-                            new ResponseHeader(
-                                    200, file.length(), "Document Follows");
-
-                    String extension = file.getName().substring(file.getName().indexOf(".") + 1);
-                    responseHeader.setContentType(ContentTypeMapper.getContentType(extension));
-                    res = new Response(fileToBytes(file), responseHeader);
-                }
-                break;
-
-            default:
-                System.out.println("default");
-                break;
+        @Override
+        public void run() {
+            handleRequest();
         }
-        return res;
-    }
 
-    private void send(Response res, DataOutputStream out) throws IOException {
-        out.writeBytes(res.getResponseHeader().toString());
-        out.write(res.getData());
-        out.close();
-    }
+        /**
+         * Listens for requests
+         */
+        private void handleRequest() {
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                Request req = createRequest(reader.readLine());
+                Response res = createResponse(req);
+                DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
 
-    /**
-     * Replace with
-     * //Pattern pattern = Pattern.compile("(\\.[^.]+)$");
-     * //Matcher matcher = pattern.matcher(filePath);
-     *
-     * @param resource
-     * @return
-     */
-    private File fetchResources(String resource) {
-        File file;
-        if (resource.lastIndexOf(".") != -1) {
-            file = new File(DOCUMENT_ROOT + resource);
-        } else {
-            file = new File(DOCUMENT_ROOT + resource + ".html");
+                send(res, out);
+                clientSocket.close();
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
-        return file;
-    }
 
-    private byte[] fileToBytes(File file) {
-        byte[] buffer = new byte[(int) file.length()];
-
-        try (FileInputStream fileInputStream = new FileInputStream(file)) {
-            fileInputStream.read(buffer);
-        } catch (IOException ex) {
-            System.err.println(ex.getMessage());
+        /**
+         * Creates a new request object from the incoming header
+         *
+         * @param header the request header
+         * @return
+         */
+        private Request createRequest(String header) {
+            String[] headerInfo = header.split(" ");
+            return new Request(headerInfo[0], headerInfo[1]);
         }
-        return buffer;
+
+        /**
+         *
+         * @param req
+         * @return
+         */
+        private Response createResponse(Request req) {
+            Response res = null;
+            switch (req.getRequestType()) {
+                case GET:
+                    File file = fetchResources(req.getResource());
+                    if (file.exists()) {
+                        ResponseHeader responseHeader =
+                                new ResponseHeader(
+                                        200, file.length(), "Document Follows");
+
+                        String extension = file.getName().substring(file.getName().indexOf(".") + 1);
+                        responseHeader.setContentType(ContentTypeMapper.getContentType(extension));
+                        res = new Response(fileToBytes(file), responseHeader);
+                    }
+                    break;
+
+                default:
+                    System.out.println("default");
+                    break;
+            }
+            return res;
+        }
+
+        private void send(Response res, DataOutputStream out) throws IOException {
+            out.writeBytes(res.getResponseHeader().toString());
+            out.write(res.getData());
+            out.close();
+        }
+
+        /**
+         * Replace with
+         * //Pattern pattern = Pattern.compile("(\\.[^.]+)$");
+         * //Matcher matcher = pattern.matcher(filePath);
+         *
+         * @param resource
+         * @return
+         */
+        private File fetchResources(String resource) {
+            File file;
+            if (resource.lastIndexOf(".") != -1) {
+                file = new File(DOCUMENT_ROOT + resource);
+            } else {
+                file = new File(DOCUMENT_ROOT + resource + ".html");
+            }
+            return file;
+        }
+
+        /**
+         *
+         *
+         * @param file
+         * @return
+         */
+        private byte[] fileToBytes(File file) {
+            byte[] buffer = new byte[(int) file.length()];
+
+            try (FileInputStream fileInputStream = new FileInputStream(file)) {
+                fileInputStream.read(buffer);
+            } catch (IOException ex) {
+                System.err.println(ex.getMessage());
+            }
+            return buffer;
+        }
     }
 }
